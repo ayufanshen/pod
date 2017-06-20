fea7546014daec58408fcbe063b47725e1ed5aaf
fanshen <fanshen@zuche.com>
2017-06-20 14:30:57 +0800

    define

---
+++ PersonCenter/Define.h
@@ -0,0 +1,18 @@
+//
+//  Define.h
+//  Pods
+//
+//  Created by 饭小团 on 2017/6/20.
+//
+//
+
+#ifndef Define_h
+#define Define_h
+
+#define UI_SCREEN_WIDTH                 ([[UIScreen mainScreen] bounds].size.width)
+#define UI_SCREEN_HEIGHT                ([[UIScreen mainScreen] bounds].size.height)
+
+#define mSINGLE_LINE_WIDTH              (1 / [UIScreen mainScreen].scale)
+#define mSINGLE_LINE_ADJUST_OFFSET      ((1 / [UIScreen mainScreen].scale) / 2)
+
+#endif /* Define_h */
